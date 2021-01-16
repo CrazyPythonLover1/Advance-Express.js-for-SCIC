0735b12c14b23739993566881581bf34d750eb1d
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-01-16 22:32:48 +0600

    create middleware folder

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const middleware = require('../middlewares/middlewares');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send("hello world");
+  return res.send("hello world");
 });
 
-router.get('/userinfo', 
-  function(req,res,next) {
-    console.log("METHOD", req.method);
-    next();
-  },
+router.get('/userinfo', middleware.user, middleware.second)
 
-  function(req, res, next) {
-    res.json({
-      username: "CrazyPythonLover1",
-      name: 'MD Mainul Islam'
-    })
-  })
 
 module.exports = router;
