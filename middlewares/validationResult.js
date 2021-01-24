93d44cf0cd39345217124fca567b914723f374a8
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-01-24 16:10:22 +0600

    validation message

--- middlewares/validationResult.js
+++ middlewares/validationResult.js
@@ -2,9 +2,11 @@ const { validationResult } = require('express-validator');
 
 module.exports.checkInvalid = function( req, res, next ) {
     let errors = validationResult(req);
+
     if (errors.isEmpty()) {
         return next();
     } else {
-        return res.status(400).json({message: errors });
+        const errMsg = errors.array().map(el => `${el.param} ${el.msg}`)
+        return res.status(400).json({message: errMsg });
     }
 }
