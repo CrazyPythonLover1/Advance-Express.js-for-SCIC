a0f7f7005f4c426a1ef747e4893fea8cbf5a5ee5
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-01-24 23:33:27 +0600

    config eslint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const userService = require("../services/user.service");
+const userService = require('../services/user.service');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
@@ -8,108 +8,122 @@ module.exports.isAuthenticated = async (req, res, next) => {
             req.headers.token,
             process.env.JWT_SECRET
         );
-        console.log("JWTTTTTTTTTTTT verified", verified);
+        console.log(
+            'JWTTTTTTTTTTTT verified',
+            verified
+        );
         if (!verified) {
-            return res
-                .status(400)
-                .json({
+            return res.
+                status(400).
+                json({
                     error: true,
                     data: null,
                     token: null,
-                    message: "user not authenticated",
+                    message: 'user not authenticated'
                 });
         }
         next();
     } catch (e) {
         console.error(e);
-        return res
-            .status(500)
-            .json({
+        return res.
+            status(500).
+            json({
                 error: e.message,
                 data: null,
                 token: null,
-                message: "Something went wrong"
+                message: 'Something went wrong'
             })
     }
 }
 
-const hashPassword = (password, saltRound) => {
-    return new Promise((resolve,reject) => {
-        bcrypt.hash(password, saltRound, (err, hash) => {
-            if (err) reject(err);
+const hashPassword = (password, saltRound) => new Promise((resolve, reject) => {
+    bcrypt.hash(
+        password,
+        saltRound,
+        (err, hash) => {
+            if (err) {
+                reject(err);
+            }
             resolve(hash);
-        });
-    });
-};
+        }
+    );
+});
 
 module.exports.register = async (req, res, next) => {
     try {
-        const {body} = req;
-        const saltRound = 10;
-        body.password = await hashPassword(body.password, saltRound);
-        const user = await userService.createUser(body); // BSON data will come form mongoDB
-        const userObj = JSON.parse(JSON.stringify(user)); // BSON data convert into JSON then JSON turn into JS object
+        const {body} = req,
+            saltRound = 10;
+        body.password = await hashPassword(
+            body.password,
+            saltRound
+        );
+        const user = await userService.createUser(body), // BSON data will come form mongoDB
+            userObj = JSON.parse(JSON.stringify(user)); // BSON data convert into JSON then JSON turn into JS object
         delete userObj.password;
 
         const token = await jwt.sign(
             {
-                data: userObj,
+                data: userObj
             },
             process.env.JWT_SECRET,
             {
-                expiresIn: "24h",
+                expiresIn: '24h'
             }
         );
 
-        return res
-            .status(200)
-            .json({
+        return res.
+            status(200).
+            json({
                 error: false,
                 data: null,
-                token: token,
-                message: "registration completed"
+                token,
+                message: 'registration completed'
             });
 
     } catch (e) {
         console.error(e);
-        return res
-            .status(500)
-            .json({
+        return res.
+            status(500).
+            json({
                 error: e,
                 data: null,
                 token: null,
-                message: "something went wrong"
-                     
+                message: 'something went wrong'
+
             });
     }
 };
 
 
-const comparePassword = (password, hash) => {
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(password, hash, (err, match) => {
-            if (err) reject(err);
+const comparePassword = (password, hash) => new Promise((resolve, reject) => {
+    bcrypt.compare(
+        password,
+        hash,
+        (err, match) => {
+            if (err) {
+                reject(err);
+            }
             resolve(match);
-        });
-    });
-};
+        }
+    );
+});
 
 module.exports.login = async (req, res, next) => {
     try {
-        const user = await userService.findUserByEmail(req.body.email);
-        const matchPassword = await comparePassword(
-            req.body.password,
-            user.password
-        );
+        const user = await userService.findUserByEmail(req.body.email),
+            matchPassword = await comparePassword(
+                req.body.password,
+                user.password
+            );
         console.log(matchPassword);
         if (!matchPassword) {
-            return res
-                .status(400)
-                .json({
+            return res.
+                status(400).
+                json({
                     error: false,
                     data: null,
                     token: null,
-                    message: " User Credential didn't match"
+                    message: ' User Credential didn\'t match'
                 });
         }
         const userObj = JSON.parse(JSON.stringify(user));
@@ -121,28 +135,28 @@ module.exports.login = async (req, res, next) => {
             },
             process.env.JWT_SECRET,
             {
-                expiresIn: "24h",
+                expiresIn: '24h'
             }
         );
 
-        return res
-            .status(200)
-            .json({
+        return res.
+            status(200).
+            json({
                 error: false,
                 data: null,
-                token: token,
-                message: "login successful",
+                token,
+                message: 'login successful'
             });
 
     } catch (e) {
         console.log(e);
-        return res
-            .status(500)
-            .json({
+        return res.
+            status(500).
+            json({
                 error: e,
                 data: null,
                 token: null,
-                message: "Something went wrong"
+                message: 'Something went wrong'
             });
     }
 };
